a00259b2089ce8865db5adc0a7a0b4f7b574fe35
vinh <kimvinh869@gmail.com>
2025-07-24 20:46:29 +0700

    Fix route definition for home page to correctly point to the abouts method

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\ProductController;
 */
 
 // Customer Routes
-Route::get('/home', [HomeController::class, 'about'])->name('home');
+Route::get('/homes', [HomeController::class, 'abouts'])->name('home'); 
 Route::get('/about', [HomeController::class, 'about'])->name('about');
 Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
 Route::post('/contact', [HomeController::class, 'sendContact'])->name('contact.send');
